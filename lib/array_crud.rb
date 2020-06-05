2aa7144fb6fbbb7cc6933db4c608386f621dc9bb
langan.kg Langan <langan.kg@gmail.com>
2020-06-05 16:28:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(cool_dogs, arrays)
- cool_dogs << "spanial"
+ cool_dogs << "sarrays!"
 end
 
 def add_element_to_start_of_array(cool_dog, element)
