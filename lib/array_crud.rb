2476757067b396d19578d241447c6b0478fa4a4d
langan.kg Langan <langan.kg@gmail.com>
2020-06-05 16:22:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
  cool_dogs = ["Poodle", "Pug", "Sausage dog", "French Bulldog"]
 end
 
-def add_element_to_end_of_array(cool_dogs)
+def add_element_to_end_of_array(cool_dogs, 4)
  cool_dogs << "Spanial"
 end
 
